65c10852b7edc57efd132603e635ba9af4994be4
yohannes <yohannesbog@gmail.com>
2020-05-02 18:12:41 -0400

    add headers and link css

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', 'views')
 
+app.use(express.static("public"));
+
+
 app.get('/cats', function(req, res) {
     res.render('cats', {
         catTitle : 'Meow',
@@ -37,6 +40,7 @@ app.get('/paramage/year', function(req, res) {
     let curruntYear = date.getFullYear();
     let yearborn = curruntYear - givenAge
     res.render('ageparam', {
+        ageParam : `Passed age is ${givenAge} which indicates`,
         yearBorn : `You were born in  ${yearborn}`,
     })
 })
