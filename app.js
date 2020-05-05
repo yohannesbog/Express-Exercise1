ca933de609824ee6f30a81e36531c741dc332ef7
yohannes <yohannesbog@gmail.com>
2020-05-05 18:14:42 -0400

    add json data and render it

--- app.js
+++ app.js
@@ -5,22 +5,34 @@ app.set('view engine', 'ejs');
 app.set('views', 'views')
 
 app.use(express.static("public"));
+app.use('/images', express.static(__dirname + '/images'));
 
-
+app.locals.animaldata = require('./public/data/animals.json');
 app.get('/cats', function(req, res) {
+    let qutes = req.query[0];
+
     res.render('cats', {
+        title: `This are ${qutes}`,
+
         catTitle : 'Meow',
     })
 })
 
 app.get('/dogs', function(req, res) {
+    let qutes = req.query[0];
+
     res.render('dogs', {
         dogTitle : 'Woof',
+      title: `This are ${qutes}`,
+
     })
 })
 
 app.get('/cats_and_dogs', function(req, res) {
+    let qutes = req.query[0];
+
     res.render('dogs', {
+        title: `This are ${qutes}`,
         catdogTitle : 'Living together',
     })
 })
@@ -28,6 +40,7 @@ app.get('/cats_and_dogs', function(req, res) {
 app.get('/param/:name', function(req, res) {
     const name = req.params.name;
     res.render('parameters', {
+        title: `This are ${name}`,
         paramTitle : `Hello ${name}`,
     })
 
@@ -35,14 +48,27 @@ app.get('/param/:name', function(req, res) {
 
 
 app.get('/paramage/year', function(req, res) {
+    let qutes = req.query[0];
     let givenAge = req.query.age;
     let date = new Date();
     let curruntYear = date.getFullYear();
     let yearborn = curruntYear - givenAge
     res.render('ageparam', {
+        title: `This are ${qutes}`,
         ageParam : `Passed age is ${givenAge} which indicates`,
         yearBorn : `You were born in  ${yearborn}`,
     })
+})
+
+
+
+app.get('/dog/lucky', function(req, res) {
+    let qutes = req.query[0];
+    res.render('pets', {
+        title: `This are ${qutes}`,
+        petTitle : `Hello Lucky`,
+    })
+
 })
 
 
